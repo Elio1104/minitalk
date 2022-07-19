ef11280da38716dd2cb1011d2d7060513d3d6c3b
alondot <alondot@student.s19.be>
2022-07-19 11:55:31 +0200

    Mandatory

--- src/client.c
+++ src/client.c
@@ -1,39 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alondot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 11:40:43 by alondot           #+#    #+#             */
+/*   Updated: 2022/07/19 11:46:37 by alondot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minitalk.h>
 
-void    ft_send_bits(int pid, char c)
+void	ft_send_bits(int pid, char c)
 {
-    int bit;
+	int	bit;
 
-    bit = 0;
-    while (bit < 8)
-    {
-        if ((c & (0x01 << bit)) != 0)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        usleep(100);
-        bit++;
-    }
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (0x01 << bit)) != 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
 }
 
-int     main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
-    int i;
+	int	pid;
+	int	i;
 
-    i = 0;
-    if (argc != 3)
-    {
-        ft_printf("Error : wrong format\n");
-        ft_printf("Try : ./client <PID> <MESSAGE>\n");
-        return (1);
-    }
-    pid = ft_atoi(argv[1]);
-    while (argv[2][i] != '\0')
-    {
-        ft_send_bits(pid, argv[2][i]);
-        i++;
-    }
-    ft_send_bits(pid, '\n');
-    return (0);
+	i = 0;
+	if (argc != 3)
+	{
+		ft_printf("Error : wrong format\n");
+		ft_printf("Try : ./client <PID> <MESSAGE>\n");
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	while (argv[2][i] != '\0')
+	{
+		ft_send_bits(pid, argv[2][i]);
+		i++;
+	}
+	ft_send_bits(pid, '\n');
+	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -1,40 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alondot <alondot@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 11:40:57 by alondot           #+#    #+#             */
+/*   Updated: 2022/07/19 11:52:22 by alondot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <minitalk.h>
 
-void    ft_signal(int signal)
+void	ft_signal(int signal)
 {
-    static int bit;
-    static int i;
+	static int	bit;
+	static int	i;
 
-    if (signal == SIGUSR1)
-        i |= (0x01 << bit);
-    bit++;
-    if (bit == 8)
-    {
-        ft_printf("%c", i);
-        bit = 0;
-        i = 0;
-    }
+	if (signal == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
+	int	pid;
 
-    (void)argv;
-    if (argc != 1)
-    {
-        ft_printf("Error : wrong format/n");
-        ft_printf("Try : ./server\n");
-        return (1);
-    }
-    pid = getpid();
-    ft_printf("PID : %d\n", pid);
-    ft_printf("Waiting for client...\n");
-    while (1)
-    {
-        signal(SIGUSR1, ft_signal);
-        signal(SIGUSR2, ft_signal);
-        pause();
-    }
-    return (0);
+	(void)argv;
+	if (argc != 1)
+	{
+		ft_printf("Error : wrong format/n");
+		ft_printf("Try : ./server\n");
+		return (1);
+	}
+	pid = getpid();
+	ft_printf("PID : %d\n", pid);
+	ft_printf("Waiting for client...\n");
+	while (1)
+	{
+		signal(SIGUSR1, ft_signal);
+		signal(SIGUSR2, ft_signal);
+		pause();
+	}
+	return (0);
 }
